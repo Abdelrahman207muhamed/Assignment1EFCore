8cba121c8a3edb22d0efac9bd32344564353d20b
user <user@DESKTOP-0FTCJBS>
2025-09-10 15:29:22 +0300

    Add project files.

---
+++ Assignment1EFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment1EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
